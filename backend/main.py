98f3a5c14146cb3d883dfa18509c7b7647dac067
Jacob Buckland <jacobbuckland1901@gmail.com>
2025-04-11 15:30:42 +0100

    main.py change

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes import suggest, crawl, debug_suggest, test_gpt
+from backend.common.models import SuggestRequest, SuggestResponse
 
 app = FastAPI()
 
