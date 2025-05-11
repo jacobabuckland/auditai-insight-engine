e16ea20de4ee7fe21d1d0ca2dfdf0e4ede764e9e
Jacob Buckland <jacobbuckland1901@gmail.com>
2025-05-11 17:19:43 +0000

    Write Prisma upsert logic to save shopDomain and accessToken

--- audit-ai/app/db.server.js
+++ audit-ai/app/db.server.js
@@ -1,9 +1,13 @@
+
 import { prisma } from "~/prisma.server";
 
 export async function saveShop(shopDomain, accessToken) {
   await prisma.shop.upsert({
     where: { shopDomain },
     update: { accessToken },
-    create: { shopDomain, accessToken },
+    create: {
+      shopDomain,
+      accessToken,
+    },
   });
 }
